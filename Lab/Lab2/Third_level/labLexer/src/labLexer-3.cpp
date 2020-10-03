81913d01c317a5f785a4c01535c9edde9cff7d8a
XT <773540317@qq.com>
2020-10-03 17:02:51 +0800

    final version

--- Lab/Lab2/Third_level/labLexer/src/labLexer-3.cpp
+++ Lab/Lab2/Third_level/labLexer/src/labLexer-3.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+#include "antlr4-runtime.h"
+#include "relop.h"
+#define MAX_LENGTH 100
+
+using namespace antlr4;
+
 int main(int argc, const char* argv[])
 {
-    std::cout << "Hello, world." << std::endl;
+    char Input[MAX_LENGTH];
+    fgets(Input, MAX_LENGTH, stdin);
+    ANTLRInputStream input(Input);
+    relop lexer(&input);
+    CommonTokenStream tokens(&lexer);
+
+    tokens.fill();
+//    for (auto token : tokens.getTokens()) {
+//        std::cout << token->toString() << std::endl;
+//    }
+
     return 0;
 }
